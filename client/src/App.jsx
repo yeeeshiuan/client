f409f11825b52d7b1a86befb13289d88e0eed115
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-06-04 23:09:47 +0800

    install default packages, and refine structure

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+    }
+    render() {
+        return (
+                <div>
+                    <p>This is my App.</p>
+                </div>
+        )
+    }
+}
+
+export default App;
