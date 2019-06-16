7c60d8ea9fd34f109b54d1dddf83a469d339ca99
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-06-16 19:44:07 +0800

    Test XMLHttpRequest CORS issue

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Main extends Component {
     constructor() {
         super();
         this.state = {
+            data : {},
         };
+
+        this.getData = this.getData.bind(this);
     }
+
+    componentDidMount() {
+    // if there is no channel name there, get the top stream channel
+        this.getData();
+    }
+
+    getData() {
+
+        const options = {
+            method: 'GET',
+            url: `https://quality.data.gov.tw/dq_download_json.php?nid=29947&md5_url=9ddc3290bbec618eadd9f53869acf542`,
+        };
+
+        axios(options)
+        .then(res => { 
+            this.setState({ data:res.data });
+            console.log(res.data);
+        })
+    }
+
     render() {
         return (
                 <div>
                     <p>This is my App.(Main)</p>
+                    
                 </div>
         )
     }
